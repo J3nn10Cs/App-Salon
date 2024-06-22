75fece90900dd1ea46e24f0bc18d331a7da11881
Jennifer <jennifer15aries@gmail.con>
2024-06-22 14:08:00 -0500

    Inicios del proyecto

---
+++ controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+    namespace Controllers;
+
+    class LoginController{
+        public static function login(){
+            echo "Desde login";
+        }
+
+        public static function logout(){
+            echo "Desde logout";
+        }
+
+        public static function olvide(){
+            echo "Desde olvidé";
+        }
+
+        public static function recuperar(){
+            echo "Desde recuperar";
+        }
+
+        public static function crear(){
+            echo "Desde crear";
+        }
+    }
